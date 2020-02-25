aa255260e4c6cf4c322e0e417e6b391515b36e78
nagata-yoshiteru <nagata.yoshiteru@g.mbox.nagoya-u.ac.jp>
2020-02-25 22:05:24 +0900

    reduce pushing msec to 30ms

--- src/auto_command_util.cpp
+++ src/auto_command_util.cpp
@@ -8,7 +8,7 @@
 #include "auto_command_util.h"
 
 // ボタンを押してから離すまでの時間
-const uint16_t BUTTON_PUSHING_MSEC = 60;
+const uint16_t BUTTON_PUSHING_MSEC = 30;
 
 // LEDに使うピン
 const uint16_t LED_PIN = 13;
